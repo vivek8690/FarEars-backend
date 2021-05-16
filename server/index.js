6fb624a90c8383b318f25d8aff9bee2c522a1aed
vivek prajapati <vivekprajapati@gofynd.com>
2021-05-16 11:22:50 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("Server is running");
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
